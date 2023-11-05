e67d104ce3bb053c4147cbb98f61e2657cd31482
Naym Hossen <naym@Nayms-MacBook-Pro.local>
2023-11-06 04:20:10 +0600

    create a jwt and set login and low out jwt function and compleate jwt function

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 //////// Middle Ware \\\\\\\\
 
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173"],
+  credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
 
@@ -24,31 +27,71 @@ const client = new MongoClient(uri, {
   },
 });
 
+
+////// Middle Ware \\\\\\\
+const logger = (req, res, next) => {
+  console.log("Log Info", req.method, req.url);
+  next();
+};
+
+const verifyToken = (req, res, next) => {
+  const token = req.cookies?.token;
+  // console.log('token in the middleware', token);
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
     const tabCategoryCollection = client.db("JobBox").collection("tabsDB");
-    const categoryCollection = client.db("JobBox").collection("categoryDB")
 
-    /// 3 Tab Name Category;
-    app.get('/api/allTabs', async (req, res) => {
-      const cursor = categoryCollection.find();
-      const result = await cursor.toArray();
-      res.send(result);
-    })
 
-    /// Tabs Data GetOne
+    /// Bids Detail Page
+    // app.get('/api/v1/tabs/:id', async(req, res) => {
+    //   const id = req.params.id;
+    //   const filter = { _id: new ObjectId(id)};
+    //   const details = await tabCategoryCollection.findOne(filter);
+    //   res.send(details);
+    // })
+
+    // Tabs Data GetOne
     app.get("/api/v1/tabs", async (req, res) => {
       const cursor = tabCategoryCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
-    app.get("/api/v1/tabs/:category", async (req, res) => {
-      const tabs = req.params.category;
-      const result = await tabCategoryCollection.find({ category: tabs }).toArray();
-      res.send(result);
+
+
+       /// Auth Related API
+    //login jwt token
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      console.log("user for tokens", user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1h" });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: "none",
+        })
+        .send({ success: true });
+    });
+
+    //Logout jwt Token
+    app.post("/logout", (req, res) => {
+      const user = req.body;
+      res.clearCookie("token", { maxAge: 0 }).send({ success: true });
     });
 
     // Send a ping to confirm a successful connection
